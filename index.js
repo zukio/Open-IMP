6d5fee2df56289806fbdf71a66bdd5e3a0370db6
zukio <as.web554@gmail.com>
2022-04-24 13:08:58 +0900

    最初のコード

---
+++ index.js
@@ -0,0 +1,23 @@
+// 大事なことリスト
+const preciousArr = ["負けない", "投げ出さない", "逃げ出さない", "信じ抜く"]
+
+// 涙を見せていいか否か
+function ArrowShowTears(CurrentStatus, memory) {
+  // 現在のステータス
+  if(CurrentStatus == "ダメになりそう"){
+    // 1番大事なことを取得
+    var mostPrecious = GetMostPrecious(CurrentStatus)
+    // 忘れなければ
+    if(memory.includes()){
+      // 涙を見せることを許可
+      return true
+    } 
+  }
+  return false
+}
+
+//TODO:1番大事なことを取得
+function GetMostPrecious(CurrentStatus){
+  // １つに絞る方法が見いだせずランダム処理しています
+  return preciousArr[Math.floor(Math.random() * preciousArr.length)];
+} 
